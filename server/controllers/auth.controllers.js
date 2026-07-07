bff85b96bcce0794e15e490e316795d1264249f0
dunnaabhishek8 <dunnaabhishek8@gmail.com>
2026-07-07 10:30:29 +0530

    Update auth.controllers.js

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -17,8 +17,8 @@ export const googleAuth=async (req,res) => {
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            samesite:"strict",
+            secure:true,
+            samesite:"none",
             maxAge:7*24*60*60*1000
         })
 
@@ -34,8 +34,8 @@ export const logOut=async (req,res) => {
     try {
         res.clearCookie('token',{
             httpOnly:true,
-            secure:false,
-            samesite:"strict"
+            secure:true,
+            samesite:"none"
         })
 
         return res.status(200).json({message:"Log out successfully"})
